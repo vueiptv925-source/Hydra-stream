f72dba4c218098d54507df57466d11da68083a20
vueiptv925-source <vueiptv925@gmail.com>
2026-07-04 13:31:30 +0100

    Update cache.js

--- src/cache.js
+++ src/cache.js
@@ -1,11 +1,10 @@
 import { providers, buildUrl } from './providers.js';
 
 export const getStreams = async (params) => {
-  const sources = providers.map((provider) => ({
+  return providers.map((provider) => ({
     id: provider.id,
     label: provider.label,
     url: buildUrl(provider, params),
     status: 'ready'
   }));
-  return sources;
 };

23d5c23a88d9e0e3704d9e0520605fa4ecbc18b9
vueiptv925-source <vueiptv925@gmail.com>
2026-07-05 16:21:12 +0100

    Update providers.js

--- src/providers.js
+++ src/providers.js
@@ -6,17 +6,38 @@ export const providers = [
   { 
     id: 'vidsrc.to', 
     label: 'VidSrc.to', 
-    buildUrl: (p) => `https://vidsrc.to/embed/${p.type}/${p.id}${p.type === 'tv' ? `/${p.season}/${p.episode}` : ''}` 
+    buildUrl: (p) => {
+      if (p.type === 'movie') {
+        return `https://vidsrc.to/embed/movie/${p.id}`;
+      } else if (p.type === 'tv') {
+        return `https://vidsrc.to/embed/tv/${p.id}/${p.season}/${p.episode}`;
+      }
+      return '';
+    }
   },
   { 
     id: 'vidsrc.pm', 
     label: 'VidSrc.pm', 
-    buildUrl: (p) => `https://vidsrc.pm/embed/${p.type}/${p.id}${p.type === 'tv' ? `/${p.season}/${p.episode}` : ''}` 
+    buildUrl: (p) => {
+      if (p.type === 'movie') {
+        return `https://vidsrc.pm/embed/movie/${p.id}`;
+      } else if (p.type === 'tv') {
+        return `https://vidsrc.pm/embed/tv/${p.id}/${p.season}/${p.episode}`;
+      }
+      return '';
+    }
   },
   { 
     id: 'vidsrc.me', 
     label: 'VidSrc.me', 
-    buildUrl: (p) => `https://vidsrc.me/embed/${p.type}/${p.id}${p.type === 'tv' ? `/${p.season}/${p.episode}` : ''}` 
+    buildUrl: (p) => {
+      if (p.type === 'movie') {
+        return `https://vidsrc.me/embed/movie/${p.id}`;
+      } else if (p.type === 'tv') {
+        return `https://vidsrc.me/embed/tv/${p.id}/${p.season}/${p.episode}`;
+      }
+      return '';
+    }
   },
   { 
     id: 'moviesapi', 
